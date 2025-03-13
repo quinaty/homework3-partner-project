9c6082a163107ac8bab20677042bf3255318b6c8
quinaty <cmwl22334455@163.com>
2025-03-13 17:27:33 +0800

    chore: add generate feature for expression

--- generate.py
+++ generate.py
@@ -26,4 +26,20 @@ class Expression:
         self.bracket = bracket
 
 
+def generate_numberic(r):
+    denominator = random.randint(0, r)
+    numerator = random.randint(1, r)
+    numberic = Numberic(denominator, numerator)
+    return numberic
 
+def generate_expression(value):
+    operator = random.choice(list(Operators))
+    bracket = random.choice(list(Brackets))
+    exp = Expression(value, operator, bracket)
+    return exp
+
+
+
+
+if __name__ == '__main__':
+    r = 10
