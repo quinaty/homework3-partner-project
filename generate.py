af838f7d1871fb7bdd7c9a7d7ed5124f57a3c897
quinaty <cmwl22334455@163.com>
2025-03-14 19:09:24 +0800

    chore: add generate feature for brackets matching

--- generate.py
+++ generate.py
@@ -2,6 +2,25 @@ import random
 import enum
 import equation as eq
 
+class Brackets_Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, bracket):
+        self.stack.append(bracket)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def length(self):
+        return len(self.stack)
+
 
 class Operators(enum.Enum):
     ADD = '+'
@@ -32,9 +51,15 @@ def generate_numberic(r):
     numberic = Numberic(denominator, numerator)
     return numberic
 
-def generate_expression(value):
-    operator = random.choice(list(Operators))
-    bracket = random.choice(list(Brackets))
+def generate_expression(value,is_root=False):
+    rootlist = [Brackets.LEFT,Brackets.NULL]
+    operator = Operators.ADD
+    bracket = random.choice(rootlist)
+
+    if not is_root:
+        operator = random.choice(list(Operators))
+        bracket = random.choice(list(Brackets))
+
     exp = Expression(value, operator, bracket)
     return exp
 
@@ -51,41 +76,58 @@ def generate_expression_node(r, current, parent):
 
     return node
 
-def generate_equation_tree(r,limit = 4):
-    root = eq.EquationTree(generate_expression(generate_numberic(r)),None,None)
-    current = root
-    bracket_count = 0
-    expression_count = 1
 
-    while True:
-        if(expression_count == limit and bracket_count != 0):
-            expression_count -= 1
-            current = current.parent
-            current.left = None
-            current.right = None
-        else:
-            if expression_count == limit and bracket_count == 0:
-                break
+def bracket_check(node, stack, remain_num):
+    if node.expn.bracket == Brackets.RIGHT and stack.is_empty():
+       return False
+    if node.expn.bracket == Brackets.LEFT and remain_num == 1:
+        return False
+
+   # print(stack.length(), remain_num)
+
+    required_num = stack.length()
+    probability = random.random()
+    if probability < required_num/remain_num:
+        node.expn.bracket = Brackets.RIGHT
+
+    return True
+
+
+def generate_equation_tree(r,limit = 4):
+    root = eq.EquationTree(generate_expression(generate_numberic(r), True),None,None)
+    current = root
+    expression_count = 1
+    stack = Brackets_Stack()
+    if root.expn.bracket == Brackets.LEFT:
+        stack.push('(')
+
+    while expression_count < limit :
 
         node = generate_expression_node(r, current, current.parent)
 
-        if node.expn.bracket == Brackets.LEFT:
-            bracket_count += 1
-        if node.expn.bracket == Brackets.RIGHT:
-            bracket_count -= 1
+        if bracket_check(node, stack, limit - expression_count):
+            if node.expn.bracket == Brackets.LEFT:
+                stack.push('(')
+            if node.expn.bracket == Brackets.RIGHT and not stack.is_empty():
+                stack.pop()
 
-        if expression_count == 1:
-            current.parent = root
+            if expression_count == 0:
+                current.parent = root
+            else:
+                node.parent = current
+
+            current = node
+            expression_count += 1
+
         else:
-         node.parent = current
-
-        current = node
-        expression_count += 1
+            current.left = None
+            current.right = None
+            continue
 
     return root
 
 
 if __name__ == '__main__':
     r = 10
-    root = generate_equation_tree(r)
-    root.traverse()
+    equation = generate_equation_tree(r)
+    equation.traverse()
