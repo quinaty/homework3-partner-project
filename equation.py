68f304dd4b7a3037406c8fdb6aed8baae9110d90
quinaty <cmwl22334455@163.com>
2025-03-16 17:42:20 +0800

    chore: modify the code of generator.py and the structure of equation tree

--- equation.py
+++ equation.py
@@ -1,35 +1,10 @@
 import enum
 
-class Brackets_Stack:
-    def __init__(self):
-        self.stack = []
-
-    def push(self, bracket):
-        self.stack.append(bracket)
-
-    def pop(self):
-        if len(self.stack) > 0:
-            return self.stack.pop()
-        else:
-            return None
-
-    def is_empty(self):
-        return len(self.stack) == 0
-
-    def length(self):
-        return len(self.stack)
-
-
 class Operators(enum.Enum):
     ADD = '+'
     SUB = '-'
     MUL = '*'
     DIV = '/'
-
-class Brackets(enum.Enum):
-    LEFT = '('
-    RIGHT = ')'
-    NULL = None
 
 class Numberic:
     def __init__(self, denominator, numerator):
@@ -39,18 +14,11 @@ class Numberic:
     def get_value(self):
         return self.numerator/self.denominator
 
-class Expression:
-    def __init__(self, value, operator , bracket=Brackets.NULL):
+class EquationNode:
+    def __init__(self, value, left, right):
         self.value = value
-        self.operator = operator
-        self.bracket = bracket
-
-class EquationTree:
-    def __init__(self, expn, left, right, parent=None):
-        self.expn = expn
         self.left = left
         self.right = right
-        self.parent = parent
 
     def add_left_child(self, child):
         self.left = child
@@ -58,43 +26,14 @@ class EquationTree:
     def add_right_child(self, child):
         self.right = child
 
-
     def traverse(self):
-        print(str(self.expn.value.denominator/self.expn.value.numerator),str(self.expn.operator),str(self.expn.bracket))
-        if self.left is not None:
+        if self.left:
             self.left.traverse()
-        if self.right is not None:
+        if self.right:
             self.right.traverse()
 
+        if type(self.value) == Numberic:
+            print(self.value.get_value())
+        else:
+            print(self.value)
 
-
-
-    def evaluate(self):
-        value = 0
-        operator = Operators.ADD
-        is_get_value = False
-        if self.left is not None:
-            value = self.left.evaluate()
-            operator = self.left.expn.operator
-            is_get_value = True
-
-
-        match operator:
-            case Operators.ADD:
-                return  self.expn.value.get_value() + value
-            case Operators.SUB:
-                return  self.expn.value.get_value() - value
-            case Operators.MUL:
-                if not is_get_value:
-                    value = 1
-                return  self.expn.value.get_value() * value
-            case Operators.DIV:
-                if not is_get_value:
-                    value = 1
-                return  self.expn.value.get_value() / value
-
-
-        if self.right is not None:
-            value = self.right.evaluate()
-            operator = self.right.expn.operator
-            is_get_value = True
--- generate.py
+++ generate.py
@@ -1,96 +1,43 @@
 import random
-import enum
 import equation as eq
+import math
 
+class Generator:
+    def __init__(self, r, limit):
+        self.r = r
+        self.limit = limit
+        self.depth = math.ceil(self.limit / 2)
 
+    def generate_numberic(self):
+        denominator = random.randint(1,self.r)
+        numerator = random.randint(0, self.r)
+        numberic = eq.Numberic(denominator, numerator)
+        return numberic
 
+    def generate_equation_tree(self,depth):
+        r = self.r
 
-def generate_numberic(r):
-    denominator = random.randint(0, r)
-    numerator = random.randint(1, r)
-    numberic = eq.Numberic(denominator, numerator)
-    return numberic
-
-def generate_expression(value,is_root=False):
-    rootlist = [eq.Brackets.LEFT,eq.Brackets.NULL]
-    operator = eq.Operators.ADD
-    bracket = random.choice(rootlist)
-
-    if not is_root:
-        operator = random.choice(list(eq.Operators))
-        bracket = random.choice(list(eq.Brackets))
-
-    exp = eq.Expression(value, operator, bracket)
-    return exp
-
-def generate_expression_node(r):
-    node = eq.EquationTree(generate_expression(generate_numberic(r)), None, None)
-    return node
-
-def node_add(node, current, parent):
-    if node.expn.operator == eq.Operators.ADD or node.expn.operator == eq.Operators.SUB or (
-            parent and parent.expn.bracket == eq.Brackets.RIGHT):
-        current.add_right_child(node)
-    else:
-        if node.expn.operator == eq.Operators.MUL or node.expn.operator == eq.Operators.DIV or (
-                node and node.expn.bracket == eq.Brackets.LEFT):
-            current.add_left_child(node)
-
-
-def bracket_check(node, stack, remain_num):
-    if node.expn.bracket == eq.Brackets.RIGHT and stack.is_empty():
-       return False
-    if node.expn.bracket == eq.Brackets.LEFT and remain_num == 1:
-        return False
-
-   # print(stack.length(), remain_num)
-
-    required_num = stack.length()
-    probability = random.random()
-    if probability < required_num/remain_num:
-        node.expn.bracket = eq.Brackets.RIGHT
-
-    return True
-
-
-def generate_equation_tree(r,limit = 4):
-    root = eq.EquationTree(generate_expression(generate_numberic(r), True),None,None)
-    current = root
-    expression_count = 1
-    stack = eq.Brackets_Stack()
-    if root.expn.bracket == eq.Brackets.LEFT:
-        stack.push('(')
-
-    while expression_count < limit :
-
-        node = generate_expression_node(r)
-
-        if bracket_check(node, stack, limit - expression_count):
-            if node.expn.bracket == eq.Brackets.LEFT:
-                stack.push('(')
-            if node.expn.bracket == eq.Brackets.RIGHT and not stack.is_empty():
-                stack.pop()
-
-            if expression_count == 0:
-                current.parent = root
-            else:
-                node.parent = current
-
-            node_add(node, current, current.parent)
-
-
-            if not current.right is None:
-                current = node
-            expression_count += 1
-
+        if depth == 0:
+            value = self.generate_numberic()
+            self.limit -= 1
+            return eq.EquationNode(value, None, None)
         else:
-            continue
+            op = random.choice(list(eq.Operators))
+            node = eq.EquationNode(op, None, None)
 
-    return root
+            if self.limit >= 0:
+                node.add_left_child(self.generate_equation_tree(depth-1))
+            else:
+                node.add_left_child(None)
 
+            if self.limit >= 0:
+                node.add_right_child(self.generate_equation_tree(depth-1))
+            else:
+                node.add_right_child(None)
+
+            return node
 
 if __name__ == '__main__':
-    r = 10
-    equation = generate_equation_tree(r)
-    equation.traverse()
-    print(equation.evaluate())
+    g = Generator(10, 4)
+    tree = g.generate_equation_tree(g.depth)
+    tree.traverse()
