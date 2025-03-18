d12c6267a54a488a865c615463c41c6d2fbba3a9
quinaty <cmwl22334455@163.com>
2025-03-18 12:05:31 +0800

    chore: add reading answer function for the file

--- file_processor.py
+++ file_processor.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+import equation as eq
 
 # 从命令行读取文件
 def read_file_from_args():
@@ -47,3 +49,35 @@ def file_write(file_path, data):
         print("错误：文件编码不支持，请尝试指定其他编码方式")
     except Exception as e:
         print(f"写入文件时发生未知错误：{str(e)}")
+
+
+def answer_read(answer_file):
+    answer_data = file_read(answer_file)
+    if answer_data is None:
+        return None
+    else:
+        answer_list = answer_data.split('\n')
+
+    pattern = re.compile(r'\d+[\'\d/\d]*?')
+    answer_set = list()
+
+    for answer in answer_list:
+        an = re.match(pattern, answer)
+        if an is None:
+            print("答案格式错误")
+            return None
+        else:
+            an = re.sub(r'[\D]', '', answer)
+            an = str(an).split('')
+            if len(an) == 1:
+                answer_set.append(eq.Numberic(1, int(an[0])))
+            elif len(an) == 2:
+                answer_set.append(eq.Numberic(int(an[1]), int(an[0])))
+            else:
+                answer_set.append(eq.Numberic(int(an[2])*int(an[0]),int(an[1])))
+
+    return answer_set
+
+
+
+
